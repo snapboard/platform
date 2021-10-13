081e0d65db33057f867d1db215b77f60ecec5e7d
Calum Moore <calum.moore@gmail.com>
2021-10-13 17:06:36 +0100

    Update auth data

--- packages/platform-core/src/types/authdata.ts
+++ packages/platform-core/src/types/authdata.ts
@@ -7,8 +7,10 @@ export interface AppTokenAuthData <AuthInputs = Record<string, any>> extends App
 
 export interface AppOAuth2Data <AuthInputs = Record<string, any>> extends AppAuthDataBase<AuthInputs> {
   type: 'oauth2'
-  refreshToken?: string|null
+  refreshToken?: string | null
+  refresh_token: string
   accessToken: string
+  access_token: string
   profile?: Record<string, any>
 }
 
