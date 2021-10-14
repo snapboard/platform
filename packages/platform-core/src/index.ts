184d8ac341c5b746f3d2a9025685c02eafd5e6c3
Calum Moore <calum.moore@gmail.com>
2021-10-14 18:34:01 +0100

    Fix type error

--- packages/platform-core/src/index.ts
+++ packages/platform-core/src/index.ts
@@ -1,7 +1,8 @@
 export type { App, AppBeforeRequestMiddleware, AppAfterResponseMiddleware } from './types/app'
 export type { AppAuth } from './types/auth'
 export type { AppAuthData, AppTokenAuthData, AppOAuth2Data } from './types/authdata'
+export type { Column } from './types/column'
 export type { AppRequest, AppFn, Bundle, Snap, SnapRequest, RequestObjectConfig } from './types/requests'
-export type { AppResource, AppResourceHandler, AppResourceHandlerParams, AppResourceHandlerResponse, AppResourceBase } from './types/resource'
+export type { AppResource, AppResourceHandler, AppResourceHandlerResponse, AppResourceBase } from './types/resource'
 
 export { handler } from './handler'
