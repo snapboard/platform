84406cc1672c4adbc88ecaaa5be350f1681b83b2
Calum Moore <calum.moore@gmail.com>
2021-11-01 13:09:18 +0000

    Export types

--- packages/errors/src/index.ts
+++ packages/errors/src/index.ts
@@ -1,2 +1,4 @@
 export { ERROR_CODES, ERROR_REASONS } from './constants'
 export { createError, SnapError } from './SnapError'
+
+export type { SnapErrorBase, SnapErrorObject, SnapErrorInput } from './SnapError'
